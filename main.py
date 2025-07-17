5e89cc4052c2f12a7c8d4f1b6c141f3fc8f9fccf
Richard Michael <rmichael@edgeofthenet.org>
2025-07-17 14:46:03 -0700

    Add the Enum (int | string) tool

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from fastmcp import FastMCP
-from fastmcp.exceptions import ResourceError, ToolError
 
-server = FastMCP("Exceptions")
+#
+# FastMCP exceptions bug
+#
 
-@server.resource("boom://boom")
-def boom_resource() -> None:
-    raise ResourceError
+# from fastmcp.exceptions import ResourceError, ToolError
 
-@server.tool()
-def boom_tool() -> None:
-    raise ToolError
+# server = FastMCP("Exceptions")
+
+#   @server.resource("boom://boom")
+#   def boom_resource() -> None:
+#       raise ResourceError
+
+#   @server.tool()
+#   def boom_tool() -> None:
+#       raise ToolError
+
+
+
+#
+# Integer enum schema bug
+#
+
+from enum import Enum
+from typing import Annotated
+from pydantic import Field
+
+class Amount(Enum):
+    #   ONE=1
+    #   TWO=2
+
+    # String works:
+    ONE="1"
+    TWO="2"
+
+
+server = FastMCP("Enum schema")
+
+@server.tool
+def annotated_enum_tool(quantity: Annotated[Amount, Field(description="The quantity annotation.")]) -> str:
+    """An annotated quantity."""
+    return f"Received {quantity}"
 
 def main():
     server.run()
