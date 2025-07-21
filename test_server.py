c0aae9373f1a220c1be845cbe195ab98b7fc591b
Richard Michael <rmichael@edgeofthenet.org>
2025-07-20 19:09:36 -0700

    Tidy test_server.py

--- test_server.py
+++ test_server.py
@@ -1,22 +1,23 @@
 import pytest
-
 from fastmcp import Client
-from fastmcp.exceptions import ToolError
 
 # from fastmcp.exceptions import ResourceError
-from fastmcp.exceptions import McpError
+from fastmcp.exceptions import McpError, ToolError
 
 import main
+
 
 @pytest.fixture
 def server():
     return main.server
+
 
 @pytest.mark.asyncio
 async def test_boom_tool(server):
     async with Client(server) as client:
         with pytest.raises(ToolError):
             await client.call_tool("boom_tool")
+
 
 @pytest.mark.asyncio
 async def test_boom_resource(server):
